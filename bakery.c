154fea0878edbfe0b02c6a66a5aaaa9aee876a8a
Mateus Salvi <mateusalvi@gmail.com>
2024-05-01 17:15:28 -0300

    Refactoring

--- bakery.c
+++ bakery.c
@@ -1,27 +1,39 @@
-#include <pthread.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <ctype.h>
+#include <bakery.h>
 
-void dormir () 
+int choosing [N_THREADS];
+int ticket [N_THREADS];
+
+void lamport_mutex_init()
+{
+  for (int i = 0; i < N_THREADS; i++)
+  {
+    choosing[i] = 0;
+    ticket[i] = 0;
+  }
+  
+}
+
+void lamport_mutex_lock (int thread_id)
+{
+
+}
+
+void lamport_mutex_unlock (int thread_id)
+{
+
+}
+
+void dormir() 
 {
   usleep((rand() % 11) * 100000); /* dormir 0, 100, 200, ..., 1000 milissegundos */
 }
 
-/* Variaveis compartilhadas */
-#define N 10
-
-int choosing [N];
-int ticket [N];
-
 int max_ticket()
 {
   int i, max = ticket[0];
-  for (i = 1; i < N; i++)
-  max = ticket[i] > max ? ticket[i] : max;
+
+  for (i = 1; i < N_THREADS; i++)
+    max = ticket[i] > max ? ticket[i] : max;
 }
 
 static void * thread_process(void *arg)
@@ -34,9 +46,10 @@ static void * thread_process(void *arg)
     choosing[i] = 1;
     ticket[i] = max_ticket () + 1;
     choosing[i] = 0;
-    for (j = 0; j < N; j++) 
+
+    for (j = 0; j < N_THREADS; j++) 
     {
-      while (choosing[j]) 
+      while (choosing[j])
         /* nao fazer nada */;
       while (ticket[j] != 0 && ((ticket[j] < ticket[i]) || (ticket[j] == ticket[i] && j < i)))
         /* nao fazer nada */;
@@ -47,35 +60,36 @@ static void * thread_process(void *arg)
     printf("I'm thread %d and I'm leaving my critical region!\n", i);
 
     ticket[i] = 0; /* indicar que saimos da secao critica */
+
   }while (1);
 
   return NULL;
 }
 
-int main(int argc, char **argv)
-{
-  int thread_num, ret;
-  pthread_t tinfo_process[N];
-  int tinfo_id[N];
-  pthread_attr_t attr;
-  void *res;
+// int main(int argc, char **argv)
+// {
+//   int thread_num, ret;
+//   int tinfo_id[N_THREADS];
+//   pthread_t tinfo_process[N_THREADS];
+//   pthread_attr_t attr;
+//   void *res;
 
-  ret = pthread_attr_init(&attr);
+//   ret = pthread_attr_init(&attr);
 
-  for (thread_num = 0; thread_num < N; thread_num++)
-  {
-    tinfo_id [thread_num] = thread_num;
-    ret = pthread_create(&tinfo_process[thread_num], &attr, &thread_process, &tinfo_id[thread_num]);
-  }
+//   for (thread_num = 0; thread_num < N_THREADS; thread_num++)
+//   {
+//     tinfo_id [thread_num] = thread_num;
+//     ret = pthread_create(&tinfo_process[thread_num], &attr, &thread_process, &tinfo_id[thread_num]);
+//   }
 
-  ret = pthread_attr_destroy(&attr);
+//   ret = pthread_attr_destroy(&attr);
 
-  for(thread_num = 0; thread_num < N; thread_num++)
-  {
-    ret = pthread_join(tinfo_process[thread_num], &res);
-    printf("Joined with thread id %d\n", thread_num);
-    free(res);
-  }
+//   for(thread_num = 0; thread_num < N_THREADS; thread_num++)
+//   {
+//     ret = pthread_join(tinfo_process[thread_num], &res);
+//     printf("Joined with thread id %d\n", thread_num);
+//     free(res);
+//   }
 
-  exit(EXIT_SUCCESS);
-}
+//   exit(EXIT_SUCCESS);
+// }
--- bakery.h
+++ bakery.h
@@ -1,4 +1,19 @@
 #ifndef _BAKERY_
 #define _BAKERY_
 
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+
+#define N_THREADS 10
+
+void lamport_mutex_init();
+void lamport_mutex_lock (int thread_id);
+void lamport_mutex_unlock (int thread_id);
+static void * thread_process(void *arg);
+
 #endif
