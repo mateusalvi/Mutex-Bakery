99c52e908b8414cb143d4afb41d898060f68ae80
Mateus Salvi <mateusalvi@gmail.com>
2024-05-01 15:24:48 -0300

    code identation

--- bakery.c
+++ bakery.c
@@ -6,7 +6,8 @@
 #include <errno.h>
 #include <ctype.h>
 
-void dormir () {
+void dormir () 
+{
   usleep((rand() % 11) * 100000); /* dormir 0, 100, 200, ..., 1000 milissegundos */
 }
 
@@ -16,61 +17,65 @@ void dormir () {
 int choosing [N];
 int ticket [N];
 
-int max_ticket() {
+int max_ticket()
+{
   int i, max = ticket[0];
   for (i = 1; i < N; i++)
-    max = ticket[i] > max ? ticket[i] : max;
+  max = ticket[i] > max ? ticket[i] : max;
 }
 
 static void * thread_process(void *arg)
 {
-    int j, i = *((int *) arg);
+  int j, i = *((int *) arg);
 
-    printf("Hello! I'm thread %d!\n", i);
+  printf("Hello! I'm thread %d!\n", i);
 
-    do {
-      choosing[i] = 1;
-      ticket[i] = max_ticket () + 1;
-      choosing[i] = 0;
-      for (j = 0; j < N; j++) {
-              while (choosing[j]) /* nao fazer nada */;
-	      while (ticket[j] != 0 && (
-				      (ticket[j] < ticket[i]) || (ticket[j] == ticket[i] && j < i)
-			)) /* nao fazer nada */;
-      }
-      
-      printf("I'm thread %d and I'm entering my critical region!\n", i);
-      dormir();
-      printf("I'm thread %d and I'm leaving my critical region!\n", i);
+  do{
+    choosing[i] = 1;
+    ticket[i] = max_ticket () + 1;
+    choosing[i] = 0;
+    for (j = 0; j < N; j++) 
+    {
+      while (choosing[j]) 
+        /* nao fazer nada */;
+      while (ticket[j] != 0 && ((ticket[j] < ticket[i]) || (ticket[j] == ticket[i] && j < i)))
+        /* nao fazer nada */;
+    }
 
-      ticket[i] = 0; /* indicar que saimos da secao critica */
-    } while (1);
+    printf("I'm thread %d and I'm entering my critical region!\n", i);
+    dormir();
+    printf("I'm thread %d and I'm leaving my critical region!\n", i);
 
-    return NULL;
+    ticket[i] = 0; /* indicar que saimos da secao critica */
+  }while (1);
+
+return NULL;
 }
 
 int main(int argc, char **argv)
 {
-    int thread_num, ret;
-    pthread_t tinfo_process[N];
-    int tinfo_id[N];
-    pthread_attr_t attr;
-    void *res;
-    
-    ret = pthread_attr_init(&attr);
-    
-    for (thread_num = 0; thread_num < N; thread_num++) {
-	    tinfo_id [thread_num] = thread_num;
-	    ret = pthread_create(&tinfo_process[thread_num], &attr, &thread_process, &tinfo_id[thread_num]);
-    }
+  int thread_num, ret;
+  pthread_t tinfo_process[N];
+  int tinfo_id[N];
+  pthread_attr_t attr;
+  void *res;
 
-    ret = pthread_attr_destroy(&attr);
-    
-    for (thread_num = 0; thread_num < N; thread_num++) {
-    	ret = pthread_join(tinfo_process[thread_num], &res);
-    	printf("Joined with thread id %d\n", thread_num);
-    	free(res);
-    }
+  ret = pthread_attr_init(&attr);
 
-    exit(EXIT_SUCCESS);
+  for (thread_num = 0; thread_num < N; thread_num++)
+  {
+    tinfo_id [thread_num] = thread_num;
+    ret = pthread_create(&tinfo_process[thread_num], &attr, &thread_process, &tinfo_id[thread_num]);
+  }
+
+  ret = pthread_attr_destroy(&attr);
+
+  for(thread_num = 0; thread_num < N; thread_num++)
+  {
+    ret = pthread_join(tinfo_process[thread_num], &res);
+    printf("Joined with thread id %d\n", thread_num);
+    free(res);
+  }
+
+  exit(EXIT_SUCCESS);
 }
